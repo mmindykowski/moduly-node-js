44891327d476a24e8b57341a6b0930efa87b1a59
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-20 21:12:08 +0200

    moduly w osobnych plikach na zach. porzadku

--- modules-es5/math/index.js
+++ modules-es5/math/index.js
@@ -1,23 +1,9 @@
-function add(...numbers) {
-  let sum = 0;
-
-  for (let number of numbers) {
-    sum += number;
-  }
-
-  return sum;
-}
-
-function multiply(...numbers) {
-  let product = 1;
-
-  for (const number of numbers) {
-    product *= number;
-  }
-  return product;
-}
+const add = require('./add')
+const multiply = require('./multiply')
 
 module.exports = {
   add: add,
   multiply: multiply,
 };
+
+
