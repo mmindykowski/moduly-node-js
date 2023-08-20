4d27110c68a7d7b1abeb491d874f1ead0009c620
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-20 20:52:27 +0200

    podpiecie obiektu o danej nazwie

--- modules-es5/index.js
+++ modules-es5/index.js
@@ -1,6 +1,8 @@
 const math = require("./math");
+const add = require("./math").add;
 
 // console.log(add);
 
 console.log(math.add(3, 4, 5));
 console.log(math.multiply(3, 4, 5));
+console.log(add(2, 2));
