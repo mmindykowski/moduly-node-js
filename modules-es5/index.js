baaa6fba05ca03daa537b0685cd7ae5ecf6f2932
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-20 21:02:33 +0200

    uproszczenie sciezki

--- modules-es5/index.js
+++ modules-es5/index.js
@@ -6,6 +6,6 @@
 // console.log(math.add(3, 4, 5));
 // console.log(math.multiply(3, 4, 5));
 // console.log(add(2, 2));
-const { add, multiply } = require("./math/math");
+const { add, multiply } = require("./math");
 
 console.log(multiply(3,10));
